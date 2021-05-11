a2ece6220d87e254fd5c3a3119e17a5811f63292
Cronos Uchoa <Gabriel5432@gmail.com>
2021-05-11 20:52:09 -0300

    Env var

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -17,7 +17,7 @@ function DataTable() {
     })
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales?page=${activePage}&size=10&sort-date,desc`)
+        axios.get(`${BASE_URL}/sales?page=${activePage  }&size=10&sort-date,desc`)
             .then(response => {
                 setPage(response.data);
             })
